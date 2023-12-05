ce5bc78006d00cb7432b62e9bd3c9626d57dc44b
VSRohodTeacher <victor.rohod@docente.senai.br>
2023-12-05 17:51:52 -0300

    aula10

--- aula10 - Jquery Animation/materialDeAula/js/script.js
+++ aula10 - Jquery Animation/materialDeAula/js/script.js
@@ -9,5 +9,34 @@ $("#animacao2").on('click', function(){
 });
 
 $("#animacao3").on('click', function(){
-    caixa.animate({margin : "0px", rotate : "-360deg"});
+    caixa.animate({margin : "0px", rotate : "-360deg"},{duration : 3000});
+});
+
+$("#animacao4").on('click', function(){
+    caixa.animate({margin : "100px",width: "600px", height: "600px"},{complete : () => {alert("Me contrata ;)")}});
+});
+
+$("#animacao5").on('click', function(){
+    // caixa.hide(500);
+    caixa.slideUp(500);
+    // caixa.fadeOut(500);
+});
+
+$("#animacao6").on('click', function(){
+    // caixa.show(500);
+    // caixa.slideDown(500);
+    caixa.fadeIn(500);
+});
+
+$("#animacao7").on('click', function(){
+    // caixa.fadeToggle(500);
+    caixa.slideToggle(500);
+});
+
+$("#animacao8").on('click', function(){
+    caixa.css("transition","1s");
+    caixa.css("backgroundColor","red");
+    $("img").attr("src","https://www.petz.com.br/blog//wp-content/uploads/2020/08/cat-cafe-pet.jpg");
+    $("img").attr("width","300px");
+    $("#teste").attr("placeholder","Mensagem legal")
 });
